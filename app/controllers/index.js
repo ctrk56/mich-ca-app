0b2660ade5238d973554874cacb364f8ee271d75
ctrk <ctrk56@gmail.com>
2017-04-13 18:58:14 -0400

    orientation lock

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 Alloy.Globals.mainTab = $.mainTab;
 Alloy.CFG.tabGroup = $.mainTab;
 $.mainTab.exitOnClose = true;
+$.mainTab.orientationModes = [Ti.UI.PORTRAIT];
 
 if (OS_ANDROID) {
     $.mainTab.addEventListener('android:back', function() {
